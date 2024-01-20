499b2e393753093e0d7eb75f0437fb04b2512ea1
bri-yan <bri.yan@outlook.com>
2024-01-20 15:47:13 -0800

    added routes

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import MapComponent from '../components/Map';
+import SideBar from '../components/SideBar';
 
-function MapPage() {
+const MapPage = (props) => {
+  const [sideBarVis, setSideBar] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModalTwoVisible, setIsModalTwoVisible] = useState(false);
+
+  const toggleSideBar = (sideBarState) => {
+    setSideBar(sideBarState);
+  }
+
   return (
-    <div className="map">
+    <div className="mapbox">
+      
+      <SideBar toggleSideBar={toggleSideBar} sideBarVis={sideBarVis} toggleThankYouModal={() => {setIsModalTwoVisible(true)}}/>
       <MapComponent sideBarVis={true} mapClick={() => {}} mapLoad={() => {}} />
     </div>
   );
