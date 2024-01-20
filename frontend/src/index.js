09e271eaedeb3c8654c37cd82f7539684390169b
bri-yan <bri.yan@outlook.com>
2024-01-20 14:08:38 -0800

    edited map

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import MapPage from './pages/MapPage';
+import MapComponent from './components/Map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Map from '../components/Map';
+import MapComponent from '../components/Map';
 
 function MapPage() {
   return (
     <div className="map">
-      <Map sideBarVis={false} mapClick={() => {}} mapLoad={() => {}} />
+      <MapComponent sideBarVis={true} mapClick={() => {}} mapLoad={() => {}} />
     </div>
   );
 }
