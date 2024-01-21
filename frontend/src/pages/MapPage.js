65e8d64e7b2087d80927e5ed47d3badb55cd82b7
bri-yan <bri.yan@outlook.com>
2024-01-20 23:44:18 -0800

    added 3d models

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -5,12 +5,12 @@ import { loadLocations } from '../actions/MapActions';
 
 const MapPage = (props) => {
   const mockFarms = {
-    'UBC Farm Market': {long: -123.23595, lat: 49.249605, address: '1234 Main St.'},
-    'Pacific Spirit Farm' : {long: -123.2095, lat: 49.245605, address: '2345 Main St.'},
+    'UBC Farm Market': {long: -123.23799, lat: 49.25105, address: '1234 Main St.'},
+    'Pacific Spirit Farm' : {long: -123.176588, lat: 49.224863, address: '2345 Main St.'},
   }
   let locations = mockFarms;
   const [sideBarVis, setSideBar] = useState(true);
-  const [coords, setCoords] = useState([-123.23395, 49.249605]);
+  const [coords, setCoords] = useState([-123.23799, 49.25105]);
 
   const mapLoad = map => {
     setTimeout(() => {
