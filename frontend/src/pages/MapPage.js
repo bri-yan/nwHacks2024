a4244c92b28d8982cd1ac5c991ef8267ddc5bd48
bri-yan <bri.yan@outlook.com>
2024-01-21 00:44:35 -0800

    updated model load-in

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -5,12 +5,13 @@ import { loadLocations } from '../actions/MapActions';
 
 const MapPage = (props) => {
   const mockFarms = {
-    'UBC Farm Market': {long: -123.23799, lat: 49.25105, address: '1234 Main St.'},
+    'UBC Farm Market': {long: -123.23799, lat: 49.25125, address: '1234 Main St.'},
     'Pacific Spirit Farm' : {long: -123.176588, lat: 49.224863, address: '2345 Main St.'},
   }
   let locations = mockFarms;
   const [sideBarVis, setSideBar] = useState(true);
-  const [coords, setCoords] = useState([-123.23799, 49.25105]);
+  const [updateMap, setUpdateMap] = useState(true);
+  const [coords, setCoords] = useState([mockFarms['UBC Farm Market'].long, mockFarms['UBC Farm Market'].lat]);
 
   const mapLoad = map => {
     setTimeout(() => {
@@ -25,6 +26,10 @@ const MapPage = (props) => {
 
   const toggleCoords = (coordinates) => {
     setCoords(coordinates);
+  }
+
+  const toggleUpdateMap = () => {
+    setUpdateMap(!updateMap);
   }
 
   const mockData = [
@@ -47,8 +52,8 @@ const MapPage = (props) => {
   const mockRanking2 = ['donation-shelter'];
   return (
     <div className="mapbox">
-      <MapComponent coords={coords} mapLoad={mapLoad} sideBarVis={sideBarVis} mapClick={() => {}} />
-      <SidebarComponent toggleCoords={toggleCoords} shelters={mockFarms} ranking={mockRanking} toggleSideBar={toggleSideBar} sideBarVis={sideBarVis}/>
+      <MapComponent updateMap={updateMap} coords={coords} mapLoad={mapLoad} sideBarVis={sideBarVis} mapClick={() => {}} />
+      <SidebarComponent toggleUpdateMap={toggleUpdateMap} toggleCoords={toggleCoords} shelters={mockFarms} ranking={mockRanking} toggleSideBar={toggleSideBar} sideBarVis={sideBarVis}/>
       
     </div>
   );
